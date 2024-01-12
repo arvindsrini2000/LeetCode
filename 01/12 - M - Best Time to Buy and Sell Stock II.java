a5ad6ac34dd362b653d14f78addb8f620c79719c
arvindsrini2000 <69394691+arvindsrini2000@users.noreply.github.com>
2024-01-12 13:36:50 +0530

    Optimal space solution

--- 01/12 - M - Best Time to Buy and Sell Stock II.java
+++ 01/12 - M - Best Time to Buy and Sell Stock II.java
@@ -25,3 +25,26 @@ class Solution {
         return profit;
     }
 }
+
+
+//Slightly different approach - We just sell when the previous day's price is lesser than current Day's - Greedy.
+//This would end up in the max profit any how.
+
+//Consider case - 1,2,3,4,5
+//In first program - we buy at 1 and sell at 5.
+//In this program - we buy at 1 sell at 2, buy at 2 and sell at 3, etc,.
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int profit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i-1]<prices[i]){
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
